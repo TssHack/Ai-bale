e1db5785330890e8e3ab6eed60b347590ae30955
Ehsan <144959137+TssHack@users.noreply.github.com>
2025-03-07 01:28:13 +0330

    Update bale.py

--- bale.py
+++ bale.py
@@ -12,6 +12,18 @@ import pytz
 bot_token = "‏‏1752263879:AR7EWOyRTpIcTXyQG7kq3ZbHFBaAyFV43rEC8krO"
 bot = Client(bot_token)
 
+channel_username = "@YourChannel"  # نام کاربری کانال خود را جایگزین کنید
+
+# تابع بررسی عضویت کاربر در کانال
+def is_user_joined(user_id):
+    url = f"https://api.bale.ai/bot{bot_token}/getChatMember?chat_id={channel_username}&user_id={user_id}"
+    response = requests.get(url)
+    data = response.json()
+    
+    if data.get("ok") and data["result"]["status"] in ["member", "administrator", "creator"]:
+        return True
+    return False
+
 # دیکشنری ذخیره وضعیت کاربران
 user_states = {}
 
@@ -269,11 +281,24 @@ inline_buttons = InlineKeyboard(
 )
 return_to_main_menu_button = InlineKeyboard([("بازگشت به منو اصلی 🏠", "return_to_main_menu")])
 
+join_button = InlineKeyboard(
+    [("🔗 عضویت در کانال", f"https://t.me/{channel_username[1:]}")]
+)
+
+
+
 # مدیریت پیام‌ها
 @bot.on_message()
 async def handle_message(message):
     chat_id = message.chat.id
     state = user_states.get(chat_id)
+
+    if not is_user_joined(user_id):
+        await message.reply(
+            "برای استفاده از ربات لطفاً ابتدا در کانال زیر عضو شوید ⬇️",
+            reply_markup=join_button
+        )
+        return
 
     if state is None:
         await message.reply("🤖 به ربات صراط خوش آمدید!\n\n✨ دستیار هوشمند اسلامی شما ✨\n\n📌 این ربات امکانات متنوعی را در اختیار شما قرار می‌دهد:", reply_markup=inline_buttons)
@@ -329,6 +354,13 @@ async def on_callback(callback_query):
 """,
             reply_markup=inline_buttons
         )
+
+    if not is_user_joined(user_id):
+        await callback_query.message.edit_text(
+            "برای استفاده از ربات لطفاً ابتدا در کانال زیر عضو شوید ⬇️",
+            reply_markup=join_button
+        )
+        return
 
     elif callback_query.data == "calculate_age":
         user_states[chat_id] = "get_birthdate"
